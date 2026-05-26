a6b96afa61fa4dd243454db6b2bf7f0ec679684c
SamBeals <sdb286@nau.edu>
2026-05-26 16:56:01 -0700

    Initial Commit

---
+++ SellMateMerchantTests/SellMateMerchantTests.swift
@@ -0,0 +1,17 @@
+//
+//  SellMateMerchantTests.swift
+//  SellMateMerchantTests
+//
+//  Created by Sam on 5/26/26.
+//
+
+import Testing
+@testable import SellMateMerchant
+
+struct SellMateMerchantTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
